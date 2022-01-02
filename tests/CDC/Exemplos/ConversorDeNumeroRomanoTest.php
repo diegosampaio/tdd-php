cfdda734d7a92e2311db122b92ade0a6a241b530
Diego Sampaio <diego.estaleiro@gmail.com>
2022-01-01 21:02:23 -0300

    Testes com números romanos

---
+++ tests/CDC/Exemplos/ConversorDeNumeroRomanoTest.php
@@ -0,0 +1,53 @@
+<?php
+namespace CDC\Exemplos;
+
+require "./vendor/autoload.php";
+
+use CDC\Exemplos\ConversorDeNumeroRomano;
+use PHPUnit_Framework_TestCase as PHPUnit;
+
+class ConversorDeNumeroRomanoTest extends PHPUnit
+{
+  public function testDeveEntenderOSimboloI()
+  {
+    $romano = new ConversorDeNumeroRomano();
+    $numero = $romano->converte("I");
+    $this->assertEquals(1, $numero);
+  }
+
+  public function testeDeveEntenderOSimboloV()
+  {
+    $romano = new ConversorDeNumeroRomano();
+    $numero = $romano->converte("V");
+
+    $this->assertEquals(5, $numero);
+  }
+
+  public function testeDeveEntenderOSimboloII()
+  {
+    $romano = new ConversorDeNumeroRomano();
+    $numero = $romano->converte("II");
+    $this->assertEquals(2, $numero);
+  }
+
+  public function testeDeveEntenderOSimboloXXII()
+  {
+    $romano = new ConversorDeNumeroRomano();
+    $numero = $romano->converte("XXII");
+    $this->assertEquals(22, $numero);
+  }
+
+  public function testeDeveEntenderOSimboloIX()
+  {
+    $romano = new ConversorDeNumeroRomano();
+    $numero = $romano->converte("IX");
+    $this->assertEquals(9, $numero);
+  }
+
+  public function testeDeveEntenderOSimboloXXIV()
+  {
+    $romano = new ConversorDeNumeroRomano();
+    $numero = $romano->converte("XXIV");
+    $this->assertEquals(24, $numero);
+  }
+}
